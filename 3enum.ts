3e22b005c7e84bacb4d5f3a8ec70717d393cfa21
Aleksandr232 <aleksrowing232@gmail.com>
2022-05-12 13:45:20 +0300

    first commit

---
+++ 3enum.ts
@@ -0,0 +1,16 @@
+enum Membership{
+    Simple,
+    Standard,
+    Premium
+}
+
+const membership=Membership.Standard
+const membershipReverse=Membership[2]
+
+enum SocialMedia{
+    Vk='VK',
+    FACEBOOK='FACEBOOK',
+    INSTAGRAM='INSTAGRAM'
+}
+
+const social = SocialMedia.INSTAGRAM
